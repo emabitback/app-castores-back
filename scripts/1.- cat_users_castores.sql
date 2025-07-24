7916fbb2e544304f830e13834b4a054fcad3f963
emanuel <emanuel>
2025-07-24 12:10:34 -0600

    add scripts folder

---
+++ scripts/1.- cat_users_castores.sql
@@ -0,0 +1,11 @@
+
+
+use db_castores;
+
+INSERT INTO `db_castores`.`users`
+(`id_user`,`email`,`name`,`pwd`,`rol`,`status`) VALUES 
+(1,"ema@gmail.com","Emanuel","123",1,1);
+INSERT INTO `db_castores`.`users`
+(`id_user`,`email`,`name`,`pwd`,`rol`,`status`) VALUES 
+(2,"juan@gmail.com","Juan","123",2,1);
+
